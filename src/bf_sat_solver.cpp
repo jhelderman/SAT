4884bcea6a8537c0207f15a2b807c9096ead2488
jhelderman <jheld2010@gmail.com>
2017-01-24 22:19:59 -0500

    added a check function with a time limit

--- src/bf_sat_solver.cpp
+++ src/bf_sat_solver.cpp
@@ -23,7 +23,7 @@ void inc_input(std::vector<bool> &x) {
 BF_SAT_Solver::BF_SAT_Solver() {}
 
 
-bool BF_SAT_Solver::check(CNF_exp exp) {
+int BF_SAT_Solver::check(CNF_exp exp) {
   // initialize loop variables
   unsigned num_literals = exp.get_num_literals();
   std::vector<bool> input(num_literals, false);
@@ -31,12 +31,44 @@ bool BF_SAT_Solver::check(CNF_exp exp) {
   while (!all(input)) {
     // check this input
     if (exp.eval(input))
-      return true;
+      return 1;
     // increment the input
     inc_input(input);
   }
   if (exp.eval(input))
-    return true;
+    return 1;
   else
-    return false;
+    return 0;
+}
+
+
+double get_time(std::chrono::steady_clock::time_point start_time) {
+  std::chrono::steady_clock::time_point now =
+    std::chrono::steady_clock::now();
+  std::chrono::duration<double> T =
+    std::chrono::duration_cast<std::chrono::duration<double> >(now - start_time);
+  return T.count();
+}
+
+
+int BF_SAT_Solver::check(CNF_exp exp, const double &time_limit) {
+  // initialize loop variables
+  unsigned num_literals = exp.get_num_literals();
+  std::vector<bool> input(num_literals, false);
+  std::chrono::steady_clock::time_point start_time =
+    std::chrono::steady_clock::now();
+  // iterate over the inputs
+  while (!all(input) && get_time(start_time) < time_limit) {
+    // check this input
+    if (exp.eval(input))
+      return 1;
+    // increment the input
+    inc_input(input);
+  }
+  if (get_time(start_time) >= time_limit)
+    return -1;
+  else if (exp.eval(input))
+    return 1;
+  else
+    return 0;
 }
--- src/bf_sat_solver.h
+++ src/bf_sat_solver.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "cnf_exp.h"
 #include <vector>
+#include <chrono>
 
 
 class BF_SAT_Solver {
  public:
   BF_SAT_Solver();
-  bool check(CNF_exp exp);
+  int check(CNF_exp exp);
+  int check(CNF_exp exp, const double &time_limit);
 };
